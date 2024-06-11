1fe4ff90dab9c09d21ae4acc94d9069451c75cf9
Adiel Rodriguez <adiel.rodriguez39@myhunter.cuny.edu>
2024-06-10 23:41:05 -0400

    CSRF_TRUSTED_ORIGINS CSRF_TRUSTED_ORIGINS

--- bike_parking_django/settings.py
+++ bike_parking_django/settings.py
@@ -66,9 +66,10 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://dot-bikerack-frontend.onrender.com',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000',]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000','https://dot-bikerack-frontend.onrender.com',]
 
 ROOT_URLCONF = 'bike_parking_django.urls'
 

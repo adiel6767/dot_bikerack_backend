9b0733dc76aef43c239fd9ee7091d1f91064d3d3
Adiel Rodriguez <adiel.rodriguez39@myhunter.cuny.edu>
2024-11-22 18:31:23 -0500

    update token lifetime

--- bike_parking_django/settings.py
+++ bike_parking_django/settings.py
@@ -172,8 +172,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
